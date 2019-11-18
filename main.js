124408c283d15fcb45c50c8d58fd45ae39bc4f57
Aleksandr Tarasov <tarasov.alexander.v@gmail.com>
2019-11-18 18:30:50 +0300

    feat: add rotate ability and keybord control

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
 import Game from './src/game.js';
+import View from './src/view.js';
+
+const root = document.querySelector('#root');
 
 const game = new Game();
+const view = new View(root, 480, 640, 20, 10);
 
 window.game = game;
+window.view = view;
+
+document.addEventListener('keydown', event => {
+  switch (event.keyCode) {
+    case 37: //left arrow
+      game.movePieceLeft();
+      view.render(game.getState());
+      break;
+    case 38: //up arrow
+      game.rotatePiece();
+      view.render(game.getState());
+      break;
+    case 39: //right arrow
+      game.movePieceRight();
+      view.render(game.getState());
+      break;
+    case 40: //down air
+    game.movePieceDown();
+    view.render(game.getState());
+    break;
+  }
+})
+
--- src/game.js
+++ src/game.js
@@ -2,40 +2,57 @@ export default class Game {
   score = 0;
   lines = 0;
   level = 0;
-  playfield = [
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-  ];
+  playfield = this.createPlayfield();
   activePiece = {
     x: 0,
     y: 0,
-    blocks: [
+    blocks : [
       [0,1,0],
       [1,1,1],
       [0,0,0],
     ]
   };
 
+  getState() {
+    const playfield = this.createPlayfield();
+    const { y: pieceY, x: pieceX, blocks } = this.activePiece;
+
+    for (let y = 0; y < this.playfield.length; y++) {
+      playfield[y] = [];
+      for (let x = 0; x < this.playfield[y].length; x++) {
+        playfield[y][x] = this.playfield[y][x];
+      }
+    }
+
+    for (let y = 0; y < blocks.length; y++) {
+      for (let x = 0; x < blocks[y].length; x++) {
+        if (blocks[y][x]) {
+          playfield[pieceY + y][pieceX + x] = blocks[y][x];
+        }
+        
+      }
+      
+    }
+
+    return {
+      playfield
+    }
+  }
+
+  createPlayfield() {
+    const playfield = [];
+    for (let y = 0; y < 20; y++) {
+      playfield[y] = [];
+      for (let x = 0; x < 10; x++) {
+        playfield[y][x] = 0;
+      }
+    }
+    return playfield;
+  }
+
   movePieceLeft() {
     this.activePiece.x -= 1;
+
     if(this.hasCollision()) {
       this.activePiece.x += 1;
     }
@@ -43,6 +60,7 @@ export default class Game {
 
   movePieceRight() {
     this.activePiece.x += 1;
+
     if(this.hasCollision()) {
       this.activePiece.x -= 1;
     }
@@ -50,27 +68,61 @@ export default class Game {
 
   movePieceDown() {
     this.activePiece.y += 1;
+
     if(this.hasCollision()) {
       this.activePiece.y -= 1;
       this.lockPiece();
     }
   };
+
+  rotatePiece() {
+    this.rotateBlocks();
+
+    if (this.hasCollision()) {
+      this.rotateBlocks(false);
+    }
+  };
+
+  rotateBlocks(clockwise = true) {
+    const blocks = this.activePiece.blocks;
+    const length = blocks.length;
+    const x = Math.floor(length / 2);
+    const y = length -1;
+
+    for (let i = 0; i < x; i++) {
+      for (let j = i; j < y - i; j++) {
+        const temp = blocks[i][j];
+        
+        if (clockwise) {
+          blocks[i][j] = blocks[y - j][i];
+          blocks[y - j][i] = blocks[y - i][y - j];
+          blocks[y - i][y - j] = blocks[j][y - i];
+          blocks[j][y - i] = temp;
+        } else {
+          blocks[i][j] = blocks[j][y - i];
+          blocks[j][y - i] = blocks[y - i][y - j];
+          blocks[y - i][y - j] = blocks[y - j][i];
+          blocks[y - j][i] = temp;
+        }
+      } 
+    }
+  }
+
   // if figure is out of bounds or crash with previous figures:
   hasCollision() {
-    const playfield = this.playfield;
     const { y: pieceY, x: pieceX, blocks } = this.activePiece;
 
     for (let y = 0; y < blocks.length; y++) {
       for (let x = 0; x < blocks[y].length; x++) {
-        if (blocks[y][x] && 
-          ((playfield[pieceY + y] === undefined || playfield[pieceY + y][pieceX + x] === undefined) ||
-          playfield[pieceY + y][pieceX + x])
+        if (
+          blocks[y][x] && 
+          ((this.playfield[pieceY + y] === undefined || this.playfield[pieceY + y][pieceX + x] === undefined) ||
+          this.playfield[pieceY + y][pieceX + x])
         ) {
           return true;
         }
       }
     }
-
     return false;
   };
   // commit the position of figure on field:
@@ -80,7 +132,7 @@ export default class Game {
     for (let y = 0; y < blocks.length; y++) {
       for (let x = 0; x < blocks[y].length; x++) {
         if (blocks[y][x]) {
-          this.playfield[pieceY + y][pieceX + x] = blocks[y][x]
+          this.playfield[pieceY + y][pieceX + x] = blocks[y][x];
         }
       }
     }
